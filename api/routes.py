d9b62c97ca37ee362b724dd0f47a4599ef960e70
MiguelSantos77 <71697750+MiguelSantos77@users.noreply.github.com>
2025-05-16 22:04:17 +0100

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -1,13 +1,27 @@
 from flask import Blueprint, request, jsonify
-
+from utils.database_manager import DatabaseManager
 api = Blueprint('api', __name__)
 
 @api.route("/info" , methods=["POST"])
 def receive_info():
     "ESP will send geral info from here"
-    pass
+    db = DatabaseManager("database.db")
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Invalid or missing JSON"}), 400
+    humidity = data.get("humidity")
+    lastWatering = data.get("lastWatering")
+
+    db.insert_info(humidity, lastWatering)
+    db.close()
+    return jsonify({"status": "received"}), 200
     
 
 @api.route("/info" , methods=["GET"])
 def get_info():
-    return jsonify({"humidity": 30, "lastWatering":"5/16/2025 20:32:00" })  
+    db = DatabaseManager("database.db")
+    info = db.get_latest_info()
+    db.close()
+
+    return jsonify(dict(info))  
