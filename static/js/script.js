86496d887d2658b3da290f028eb536d84344ef15
MiguelSantos77 <71697750+MiguelSantos77@users.noreply.github.com>
2025-05-21 16:14:40 +0100

    water drop animation

--- static/js/script.js
+++ static/js/script.js
@@ -4,10 +4,22 @@ async function fetchInfo() {
         const data = await response.json();
 
         document.getElementById('humidity').textContent = data.humidity;
+        animateWaterDrop(data.humidity)
         document.getElementById('lastWatering').textContent = data.lastWatering;
     } catch (error) {
         console.error("Error:", error);
     }
+}
+
+function animateWaterDrop(percent) {
+    const rect = document.getElementById('waterLevel');
+    percent = Math.max(0, Math.min(100, percent));
+
+    const maxHeigth = 150; 
+    const waterLevel = maxHeigth * (percent / 100);
+
+    rect.setAttribute('y', maxHeigth - waterLevel);
+    rect.setAttribute('height', waterLevel);
 }
 
 fetchInfo();
